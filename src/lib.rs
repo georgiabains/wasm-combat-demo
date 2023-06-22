923efbbd4b452c972d7f7c4fa52f3b9194dc8a51
georgiabains <georgiabains@hotmail.co.uk>
2023-06-22 12:57:43 +0100

    Create entity struct in rust, and create two entities in js

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,27 @@ extern {
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
 }
+
+#[wasm_bindgen]
+pub struct Entity {
+    hp: u32,
+    attack: u32,
+}
+
+#[wasm_bindgen]
+impl Entity {
+    pub fn new(hp_val: u32, attack_val: u32) -> Entity {
+        Entity {
+            hp: hp_val,
+            attack: attack_val,
+        }
+    }
+
+    pub fn get_hp(&self) -> u32 {
+        self.hp
+    }
+
+    pub fn get_attack(&self) -> u32 {
+        self.attack
+    }
+}
--- www/src/index.js
+++ www/src/index.js
@@ -1,1 +1,10 @@
-console.log('test')
+import { Entity } from "../../pkg/wasm_combat_demo";
+
+
+const npc = Entity.new(15, 15)
+console.log(npc)
+console.log(npc.get_hp(), npc.get_attack())
+
+const player = Entity.new(30, 30)
+console.log(player)
+console.log(player.get_hp(), player.get_attack())
