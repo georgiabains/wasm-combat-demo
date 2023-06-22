abc513628e4fe43fe73e918495330029347f409c
georgiabains <georgiabains@hotmail.co.uk>
2023-06-22 18:26:44 +0100

    Set up html elements that represent a player and creature with health

--- www/src/index.js
+++ www/src/index.js
@@ -1,12 +1,32 @@
 import init, { Entity } from "../../pkg/wasm_combat_demo";
 
+const creatureSelectors = {
+  healthCurrent: '[data-creature="health-current"]',
+  healthMax: '[data-creature="health-max"]',
+  healthProgress: '[data-creature="health-progress"]'
+}
+
+const playerSelectors = {
+  attackButton: '[data-player="attack-button"]',
+  healthCurrent: '[data-player="health-current"]',
+  healthMax: '[data-player="health-max"]',
+  healthProgress: '[data-player="health-progress"]'
+}
+
 init().then(() => {
-  const npc = Entity.new(15, 15, 'creature')
-  console.log(npc)
-  console.log(npc.get_hp(), npc.get_attack(), npc.get_name())
-  
+  const creature = Entity.new(15, 15, 'creature')
   const player = Entity.new(30, 30, 'player')
-  console.log(player)
-  console.log(player.get_hp(), player.get_attack(), player.get_name())
+
+  setCreatureHealth(creature.get_hp())
+  setPlayerHealth(player.get_hp())
 })
 
+const setCreatureHealth = (health) => {
+  document.querySelector(creatureSelectors.healthCurrent).insertAdjacentText('afterbegin', health)
+  document.querySelector(creatureSelectors.healthMax).insertAdjacentText('afterbegin', health)
+}
+
+const setPlayerHealth = (health) => {
+  document.querySelector(playerSelectors.healthCurrent).insertAdjacentText('afterbegin', health)
+  document.querySelector(playerSelectors.healthMax).insertAdjacentText('afterbegin', health)
+}
