8ee8856c3c0d8c2568621873e3bcc43ddf6b5e6e
georgiabains <georgiabains@hotmail.co.uk>
2023-06-22 16:35:49 +0100

    Use init() when referencing wasm module

--- www/src/index.js
+++ www/src/index.js
@@ -1,10 +1,12 @@
-import { Entity } from "../../pkg/wasm_combat_demo";
+import init, { Entity } from "../../pkg/wasm_combat_demo";
 
+init().then(() => {
+  const npc = Entity.new(15, 15, 'creature')
+  console.log(npc)
+  console.log(npc.get_hp(), npc.get_attack(), npc.get_name())
+  
+  const player = Entity.new(30, 30, 'player')
+  console.log(player)
+  console.log(player.get_hp(), player.get_attack(), player.get_name())
+})
 
-const npc = Entity.new(15, 15, 'creature')
-console.log(npc)
-console.log(npc.get_hp(), npc.get_attack(), npc.get_name())
-
-const player = Entity.new(30, 30, 'player')
-console.log(player)
-console.log(player.get_hp(), player.get_attack(), player.get_name())
