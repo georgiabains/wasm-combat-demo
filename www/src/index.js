de106fea35cb3718b04dca60b0deb93b34670b60
georgiabains <georgiabains@hotmail.co.uk>
2024-09-08 00:09:50 +0100

    Modified code logic for setting entity health

--- www/src/index.js
+++ www/src/index.js
@@ -1,39 +1,64 @@
 import init, { Entity } from "../../pkg/wasm_combat_demo";
 
-const creatureSelectors = {
-  healthCurrent: '[data-creature="health-current"]',
-  healthTotal: '[data-creature="health-total"]',
-  healthProgress: '[data-creature="health-progress"]'
-}
-
-const playerSelectors = {
-  attackButton: '[data-player="attack-button"]',
-  healthCurrent: '[data-player="health-current"]',
-  healthTotal: '[data-player="health-total"]',
-  healthProgress: '[data-player="health-progress"]'
+const selectors = {
+  creature: {
+    healthCurrent: '[data-creature="health-current"]',
+    healthTotal: '[data-creature="health-total"]',
+    healthProgress: '[data-creature="health-progress"]'
+  },
+  player: {
+    attackButton: '[data-player="attack-button"]',
+    healthCurrent: '[data-player="health-current"]',
+    healthTotal: '[data-player="health-total"]',
+    healthProgress: '[data-player="health-progress"]'
+  }
 }
 
 init().then(() => {
   const creature = Entity.new(30, 30, 5, 'creature')
   const player = Entity.new(30, 30, 5, 'player')
 
-  setCreatureHealth(creature.get_current_hp(), creature.get_total_hp())
-  setPlayerHealth(player.get_current_hp(), player.get_total_hp())
+  initEntity('creature', creature)
+  initEntity('player', player)
 
-  document.querySelector(playerSelectors.attackButton).addEventListener('click', () => {
-    console.log('test')
-
+  document.querySelector(selectors.player.attackButton).addEventListener('click', () => {
     creature.set_current_hp(player.get_attack())
-    setCreatureHealth(creature.get_current_hp(), creature.get_total_hp())
+    setEntityCurrentHealth('creature', creature.get_current_hp())
   })
 })
 
-const setCreatureHealth = (current, total) => {
-  document.querySelector(creatureSelectors.healthCurrent).textContent = current
-  document.querySelector(creatureSelectors.healthTotal).textContent = total
+/**
+ * Initialise entity using ID & entity.
+ * @param {String} entityId - Entity reference in `selectors`
+ * @param {Entity} entity - Entity object
+ */
+function initEntity(entityId, entity) {
+  setEntityCurrentHealth(entityId, entity.get_current_hp())
+  setEntityTotalHealth(entityId, entity.get_total_hp())
 }
 
-const setPlayerHealth = (current, total) => {
-  document.querySelector(playerSelectors.healthCurrent).textContent = current
-  document.querySelector(playerSelectors.healthTotal).textContent = total
+/**
+ * Set current health of entity.
+ * @param {String} entityId - Entity reference in `selectors`
+ * @param {Number} currentHealth - Current health value
+ */
+function setEntityCurrentHealth(entityId, currentHealth) {
+  if (!selectors[entityId]) {
+    return
+  }
+
+  document.querySelector(selectors[entityId].healthCurrent).textContent = currentHealth
+}
+
+/**
+ * Set total health of entity.
+ * @param {String} entityId - Entity reference in `selectors`
+ * @param {Number} totalHealth - Total health value
+ */
+function setEntityTotalHealth(entityId, totalHealth) {
+  if (!selectors[entityId]) {
+    return
+  }
+
+  document.querySelector(selectors[entityId].healthTotal).textContent = totalHealth
 }
