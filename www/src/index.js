cf5ebfc6786b1ecf68115df7fcbbf24e6605e9c1
georgiabains <georgiabains@hotmail.co.uk>
2024-09-07 23:58:37 +0100

    Set up method for entity to take damage (in this case done by Player)

--- www/src/index.js
+++ www/src/index.js
@@ -2,31 +2,38 @@ import init, { Entity } from "../../pkg/wasm_combat_demo";
 
 const creatureSelectors = {
   healthCurrent: '[data-creature="health-current"]',
-  healthMax: '[data-creature="health-max"]',
+  healthTotal: '[data-creature="health-total"]',
   healthProgress: '[data-creature="health-progress"]'
 }
 
 const playerSelectors = {
   attackButton: '[data-player="attack-button"]',
   healthCurrent: '[data-player="health-current"]',
-  healthMax: '[data-player="health-max"]',
+  healthTotal: '[data-player="health-total"]',
   healthProgress: '[data-player="health-progress"]'
 }
 
 init().then(() => {
-  const creature = Entity.new(15, 15, 'creature')
-  const player = Entity.new(30, 30, 'player')
+  const creature = Entity.new(30, 30, 5, 'creature')
+  const player = Entity.new(30, 30, 5, 'player')
 
-  setCreatureHealth(creature.get_hp())
-  setPlayerHealth(player.get_hp())
+  setCreatureHealth(creature.get_current_hp(), creature.get_total_hp())
+  setPlayerHealth(player.get_current_hp(), player.get_total_hp())
+
+  document.querySelector(playerSelectors.attackButton).addEventListener('click', () => {
+    console.log('test')
+
+    creature.set_current_hp(player.get_attack())
+    setCreatureHealth(creature.get_current_hp(), creature.get_total_hp())
+  })
 })
 
-const setCreatureHealth = (health) => {
-  document.querySelector(creatureSelectors.healthCurrent).insertAdjacentText('afterbegin', health)
-  document.querySelector(creatureSelectors.healthMax).insertAdjacentText('afterbegin', health)
+const setCreatureHealth = (current, total) => {
+  document.querySelector(creatureSelectors.healthCurrent).textContent = current
+  document.querySelector(creatureSelectors.healthTotal).textContent = total
 }
 
-const setPlayerHealth = (health) => {
-  document.querySelector(playerSelectors.healthCurrent).insertAdjacentText('afterbegin', health)
-  document.querySelector(playerSelectors.healthMax).insertAdjacentText('afterbegin', health)
+const setPlayerHealth = (current, total) => {
+  document.querySelector(playerSelectors.healthCurrent).textContent = current
+  document.querySelector(playerSelectors.healthTotal).textContent = total
 }
